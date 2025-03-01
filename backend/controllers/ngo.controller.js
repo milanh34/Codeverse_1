8f85305e09ca971899f1facecce45fc5d572d304
milanh34 <milanh344@gmail.com>
2025-03-01 23:01:01 +0530

    Minor backend changes

--- backend/controllers/ngo.controller.js
+++ backend/controllers/ngo.controller.js
@@ -9,6 +9,7 @@ import { Post } from "../models/post.model.js";
 import { s3Upload } from "../lib/s3.js";
 import { cookieOptions } from "../constants/cookie-options.js";
 import { Donation } from "../models/donation.model.js";
+import { Volunteer } from "../models/volunteer.model.js";
 
 export const newNGO = TryCatch(async (req, res, next) => {
   const {
@@ -127,7 +128,7 @@ export const changePassword = TryCatch(async (req, res, next) => {
     return next(new ErrorHandler("Incorrect old password", 400));
   }
 
-  ngo.password = await bcrypt.hash(newPassword, 10);
+  ngo.password = newPassword;
   await ngo.save();
 
   res.status(200).json({
@@ -233,56 +234,49 @@ export const handleVolunteerRequest = TryCatch(async (req, res, next) => {
   request.status = action === "accept" ? "accepted" : "rejected";
   await request.save();
 
-  // Create notification
-  const notificationContent =
-    action === "accept"
+  try {
+    // Create notification content
+    const notificationContent = action === "accept"
       ? `Your volunteer request for ${request.event.name} has been accepted!`
       : `Your volunteer request for ${request.event.name} has been rejected.`;
 
-  // Check if a notification document exists for the user
-  let userNotification = await Notification.findOne({ user: request.user._id });
+    // Create or update notification
+    await Notification.findOneAndUpdate(
+      { user: request.user._id },
+      {
+        $push: {
+          notifications: {
+            content: notificationContent,
+            type: "event",
+            isRead: false,
+            createdAt: new Date()
+          }
+        }
+      },
+      { upsert: true, new: true }
+    );
 
-  if (!userNotification) {
-    // If no notification document exists, create a new one
-    userNotification = new Notification({
-      user: request.user._id,
-      notifications: [
-        {
-          content: notificationContent,
-          type: "event",
-          isRead: false,
-        },
-      ],
+    // If accepted, create volunteer document and add user to event participants
+    if (action === "accept") {
+      await Promise.all([
+        Volunteer.create({
+          user: request.user._id,
+          event: request.event._id,
+        }),
+        Event.findByIdAndUpdate(request.event._id, {
+          $addToSet: { participants: request.user._id },
+        }),
+      ]);
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `Request ${action}ed successfully`,
+      request,
     });
-  } else {
-    // If notification document exists, add new notification to array
-    userNotification.notifications.push({
-      content: notificationContent,
-      type: "event",
-      isRead: false,
-    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
   }
-
-  await userNotification.save();
-
-  // If accepted, create volunteer document and add user to event participants
-  if (action === "accept") {
-    await Promise.all([
-      Volunteer.create({
-        user: request.user._id,
-        event: request.event._id,
-      }),
-      Event.findByIdAndUpdate(request.event._id, {
-        $addToSet: { participants: request.user._id },
-      }),
-    ]);
-  }
-
-  res.status(200).json({
-    success: true,
-    message: `Request ${action}ed successfully`,
-    request,
-  });
 });
 
 export const createPost = TryCatch(async (req, res, next) => {
--- backend/models/notification.model.js
+++ backend/models/notification.model.js
@@ -1,31 +1,33 @@
 import mongoose, { Schema } from "mongoose";
 
 const notificationSchema = new Schema(
-    {
-        user: {
-            type: Schema.Types.ObjectId, 
-            ref: "User", 
-            required: true 
-        },
-        notifications: 
-            {
-                content: { 
-                    type: String, 
-                    required: true 
-                },
-                type: {
-                    type: String,
-                    enum: ["post", "event", "message"],
-                    required: true,
-                },
-                isRead: { 
-                    type: Boolean, 
-                    default: false 
-                },
-            }
-        
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    { timestamps: true }
+    notifications: [{
+      content: {
+        type: String,
+        required: true
+      },
+      type: {
+        type: String,
+        enum: ["event", "donation", "general"],
+        default: "general"
+      },
+      isRead: {
+        type: Boolean,
+        default: false
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    }]
+  },
+  { timestamps: true }
 );
 
 export const Notification = mongoose.model("Notification", notificationSchema);
